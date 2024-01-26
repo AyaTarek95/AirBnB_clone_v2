e3804fc391c2ba62ae23a25b17ce6cb9c7d42030
AyaTarek95 <aya.tarek213@icloud.com>
2024-01-26 04:23:57 -0800

    fix0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
@@ -6,5 +9,6 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(port=5000, host='0.0.0.0')
