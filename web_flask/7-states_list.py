92ce7f75f83570771a97e708e942a6af7673732c
AyaTarek95 <aya.tarek213@icloud.com>
2024-01-27 06:59:56 -0800

    task8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """ remove the current SQLAlchemy Session
+    After each request
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """displays the list of all State objects
+    present in DBStorage sorted by name
+    """
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
